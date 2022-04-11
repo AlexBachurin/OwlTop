6e26dda37f24dc6126c154750ed456c7a1530584
Alex <alexbachurin91@gmail.com>
2022-04-11 16:14:20 +0300

    added props to button element

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,14 @@ import React, { FC } from "react";
 import ButtonProps from "./Button.props";
 import styles from "./Button.module.css";
 import cn from "classnames";
-const Button: FC<ButtonProps> = ({ children, appearance }) => {
+const Button: FC<ButtonProps> = ({ children, appearance, ...props }) => {
 	return (
 		<button
 			className={cn(styles.btn, {
 				[styles.primary]: appearance === "primary",
 				[styles.ghost]: appearance === "ghost",
 			})}
+			{...props}
 		>
 			{children}
 		</button>
